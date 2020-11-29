3e7027999359f14c8d259b5f3c77f24994251529
melvyn2 <melvyn.depeyrot@gmail.com>
2020-11-28 16:04:17 -0800

    Use notifications, stricter errors, standard naming/better parsing

--- bs-oneclick-install.sh
+++ bs-oneclick-install.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # installs the scripts and what not
 #set -x
-cp bs-oneclick-temp bs-oneclick.sh
-cp playlist-installer-temp playlist-installer
-cp bs-oneclick-desktop-temp bs-oneclick.desktop
-
-mkdir -p ~/.local/share/applications
-xdg-mime default bs-oneclick.desktop x-scheme-handler/beatsaver x-scheme-handler/modelsaber x-scheme-handler/bsplaylist
+set -e
 
 bs_install="$HOME/.local/share/Steam/steamapps/common/Beat Saber"
 
-zenity --info --title="OneClick Installer" --text="Please navigate to your Beat Saber install directory to properly set up the OneClick Installer." --width=500
+zenity --info --title="OneClick Installer" --text="Please navigate to your Beat Saber install directory to properly set up the OneClick Installer."
 
 while : ; do
-	bs_install=$( zenity --file-selection --directory --title="Please navigate to your Beat Saber install directory." )
+	bs_install=$( zenity --file-selection --directory --title="Please navigate to your Beat Saber install directory." --filename="$HOME/.local/share/Steam/steamapps/common/Beat Saber")
 	if [[ -f "$bs_install/Beat Saber.exe" ]]; then 
+		cp bs-oneclick-temp bs-oneclick.sh
+		cp playlist-installer-temp playlist-installer
+		cp bs-oneclick-desktop-temp bs-oneclick.desktop
+
+		mkdir -p ~/.local/share/applications
+		xdg-mime default bs-oneclick.desktop x-scheme-handler/beatsaver x-scheme-handler/modelsaber x-scheme-handler/bsplaylist
 		# welcome to slash hell
 		bs_install_sed=$( echo $bs_install | sed 's,/,\\/,g' )
 		sed -i "s,bs_install=.*,bs_install=\"$bs_install_sed\"," bs-oneclick.sh
@@ -26,5 +27,5 @@ while : ; do
 		break
 	fi
 	
-	zenity --error --title="OneClick Installer" --text="Beat Saber installation not detected in that folder. Try again." --width=500
+	zenity --error --title="OneClick Installer" --text="Beat Saber installation not detected in that folder. Try again."
 done
