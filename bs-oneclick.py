493b941e87b694c0c8f8ccb54029e50a9ed1bfb1
Chris Allan <chrisallan@protonmail.com>
2021-12-21 09:43:31 -0800

    Fix maps with slash in name

--- bs-oneclick.py
+++ bs-oneclick.py
@@ -76,7 +76,7 @@ def song_dl(key: str, status: SongDLStatus = None):
     song_name = info["_songName"]
     author = info["_levelAuthorName"]
     os.chdir("..")
-    os.rename(song_dir, f"{key} ({song_name} - {author})")
+    os.rename(song_dir, f"{key} ({song_name} - {author})".replace("/", "_"))
 
     if status: 
         status.finish.set()
